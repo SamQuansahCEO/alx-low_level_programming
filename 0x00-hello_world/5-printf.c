68a6f3aa016f230c50d9f4e5b82c0e37ff5ed638
SamQuansahCEO <samquansah0112@gmail.com>
2022-06-16 04:58:00 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main Entry
+ * message
+ * return 
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+
+}
+	
+

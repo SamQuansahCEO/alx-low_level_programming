6d0f56a2fadb290d4d40d7a51dfd450390d1cd23
SamQuansahCEO <samquansah0112@gmail.com>
2022-07-05 17:25:44 -0700

    isLower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower -description, line
+ * @c: container for value to be compared
+ * Retur: always 0
+ */
+int _islower(int c)
+{
+	if (c > a 'a' && c < 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

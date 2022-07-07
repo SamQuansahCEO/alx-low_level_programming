1b35843093c90bb0ab75adc299d3eadfca58bc51
SamQuansahCEO <samquansah0112@gmail.com>
2022-07-06 17:57:35 -0700

    add-numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - prints addition of two numbers
+ * @i: print int i
+ * @k: print int k
+ * Return: Always 0.
+ */
+
+int add(int i, int k)
+{
+	return (i + k);
+}
